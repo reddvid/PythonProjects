9e68222a7c13d52bf871a6be5a7ac75d91e0f3f1
David Ballesteros <david_ballesteros@outlook.com>
2019-09-16 17:49:32 +0800

    update hello.py Co-authored-by: lanceico

---
+++ hello.py
@@ -0,0 +1,17 @@
+#/usr/bin/env python
+#working with variables, inputs, and printing
+#simple program that asks user's name then greets them
+name = input('enter your name: ') #<- First line ng code
+#gawa ka ng input na magtanong ng age---ok kuya gets ko
+age = input('your age: ') #ganyan po kuya?? - oo, try mo tapusin, malalaman kung tama ung code pag ng F5 hehe
+address = input('your address: ')
+hobby = input('your hobby: ')
+
+print(f'==== Your output ====')
+print(f'Hello {name}! You are {age} years old.')#-- ganyan parin kuya?? o name ko na, ganyan kasi kukunin nya yung VALUE ng name input, kung ano nakalagay sa { }
+print(f'Your {address} here.') #ganyan ba kuya?? - try natin mamaya haha
+print(f'About your {hobby}') 
+print(f'=====================')
+
+#check mo output mo HAHA- yung nasa taas check ko?? -- oo, ayusin mo sentence haha
+#yung ginawa ko diba kuya? Yes yes-- Ok na po yan haha
